7d98278a980f87cefd3484d85443231deb90b6a4
Tolettumm <manuel.toledo.vazquez@gmail.com>
2019-02-08 12:45:47 +0100

    normalizando el texto y comentandolo

--- coetusIV/src/main/java/com/example/spring/model/Persona.java
+++ coetusIV/src/main/java/com/example/spring/model/Persona.java
@@ -2,71 +2,132 @@ package com.example.spring.model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+/**
+ * Clase Persona
+ * 
+ * Contiene informacion de cada persona
+ * @author ManuelToledo
+ * @version 1.0
+ */
 @Entity
 @Table(name = "persona") // nombre transitorio para la tabla
 public class Persona {
 
-	private int idpersona;
+	/**
+	 * El id de la persona, que es la PrimaryKey en la tabla
+	 */
+	private int idPersona;
+	/**
+	 * Nombre de la Persona
+	 */
 	private String nombre;
+	/**
+	 * Primer apellido de la persona
+	 */
 	private String apellido1;
+	/**
+	 * Segundo apellido de la persona
+	 */
 	private String apellido2;
+	/**
+	 * DNI propiedad de la persona
+	 */
 	private String dni;
-	private Date fechanacimiento; // también podria haber importado en vez de java.utils sql
+	/**
+	 * Fecha de nacimiento de la persona
+	 */
+	private Date fechaNacimiento; // también podria haber importado en vez de java.utils sql
 
+	/**
+	 * Variable que es la PK, se autoincrementa y no puede ser null
+	 * @return el id de la persona
+	 */
 	@Id
 	@GeneratedValue
-//@Column Obada me ha dicho que lo quite, pero lo comento
+	@Column (name = "idpersona")
 	public int getIdpersona() {
-		return idpersona;
+		return idPersona;
 	}
-
+/**
+ * @param idpersona
+ */
 	public void setIdpersona(int idpersona) {
-		this.idpersona = idpersona;
+		this.idPersona = idpersona;
 	}
-
+/**
+ * 
+ * @return nombre de la persona
+ */
 	public String getNombre() {
 		return nombre;
 	}
-
+/**
+ * 
+ * @param nombre setea el nombre de la persona
+ */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
+/**
+ * 
+ * @return devuelve el primer apellido
+ */
 	public String getApellido1() {
 		return apellido1;
 	}
-
+/**
+ * 
+ * @param apellido1 setea el primer apellido
+ */
 	public void setApellido1(String apellido1) {
 		this.apellido1 = apellido1;
 	}
-
+/**
+ * 
+ * @return return el segundo apellido
+ */
 	public String getApellido2() {
 		return apellido2;
 	}
-
+/**
+ * 
+ * @param apellido2 setea el segundo apellido
+ */
 	public void setApellido2(String apellido2) {
 		this.apellido2 = apellido2;
 	}
-
+/**
+ * 
+ * @return devuelve el dni
+ */
 	public String getDni() {
 		return dni;
 	}
-
+/**
+ * 
+ * @param dni setea el dni
+ */
 	public void setDni(String dni) {
 		this.dni = dni;
 	}
-
+/**
+ * 
+ * @return la fecha de nacimiento
+ */
 	public Date getFechanacimiento() {
-		return fechanacimiento;
+		return fechaNacimiento;
 	}
-
+/**
+ * 
+ * @param fechanacimiento setea la fecha de nacimiento
+ */
 	public void setFechanacimiento(Date fechanacimiento) {
-		this.fechanacimiento = fechanacimiento;
+		this.fechaNacimiento = fechanacimiento;
 	}
 
 }
