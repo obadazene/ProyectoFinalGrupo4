f729ef8fecab0484383e6b2902891beee8b3d5f3
paulaanaya <paulicanayag98@gmail.com>
2019-02-08 11:58:17 +0100

    Editar PruebasUnitarias

--- coetusIV/src/test/java/com/example/demo/pruebas/PruebasUnitarias.java
+++ coetusIV/src/test/java/com/example/demo/pruebas/PruebasUnitarias.java
@@ -1,5 +1,32 @@
 package com.example.demo.pruebas;
 
-public class PruebasUnitarias {
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+
+import com.example.spring.dao.Repositorio;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PruebasUnitarias {
+	private static Logger logger;
+	
+	@Autowired
+	private Repositorio repositorio;
+	
+	//La anotación @Test marca el siguiente método como una prueba JUnit. 
+	@Test
+	public void delContacto(int id) {
+		
+		
+		
+		
+		
+	}
+	 
+	
 }
