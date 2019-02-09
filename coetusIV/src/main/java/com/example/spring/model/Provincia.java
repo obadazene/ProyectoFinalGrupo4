c87915a6c0cf36618e0fb6b70fbb99af94ab86a7
paulaanaya <paulicanayag98@gmail.com>
2019-02-09 19:37:19 +0100

    Correcciones, formateos y toString() en las clases Provincia y Telefono

--- coetusIV/src/main/java/com/example/spring/model/Provincia.java
+++ coetusIV/src/main/java/com/example/spring/model/Provincia.java
@@ -8,23 +8,28 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+/**
+ * 
+ * @author Grupo4
+ *
+ */
 @Entity
 @Table(name = "provincia")
 public class Provincia {
 	private int idprovincia;
 	private String provincia;
-	
-	//Persona object
-	@OneToMany(mappedBy = "provencia")
+
+	// objeto persona
+	@OneToMany(mappedBy = "provincia")
 	private List<Persona> personas;
-	//objeto dirreccion
-	@OneToMany(mappedBy = "prvincia")
-	private List<Direccion> dericciones;
 	
+	// objeto direccion
+	@OneToMany(mappedBy = "provincia")
+	private List<Direccion> direcciones;
 
 	@Id
 	@GeneratedValue
-//@Column Obada me ha dicho que lo quite, pero lo comento
+	// @Column Obada me ha dicho que lo quite, pero lo comento
 	public int getIdprovincia() {
 		return idprovincia;
 	}
@@ -39,6 +44,21 @@ public class Provincia {
 
 	public void setProvincia(String provincia) {
 		this.provincia = provincia;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Provincia [idprovincia=");
+		builder.append(idprovincia);
+		builder.append(", provincia=");
+		builder.append(provincia);
+		builder.append(", personas=");
+		builder.append(personas);
+		builder.append(", direcciones=");
+		builder.append(direcciones);
+		builder.append("]");
+		return builder.toString();
 	}
 
 }
