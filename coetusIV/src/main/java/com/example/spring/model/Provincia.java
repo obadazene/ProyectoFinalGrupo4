3fede794ba38aecf804841b5e7f1db70bea24e84
obadazene <obada.zene@hotmail.com>
2019-02-09 17:22:48 +0100

    making it more clean

--- coetusIV/src/main/java/com/example/spring/model/Provincia.java
+++ coetusIV/src/main/java/com/example/spring/model/Provincia.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
