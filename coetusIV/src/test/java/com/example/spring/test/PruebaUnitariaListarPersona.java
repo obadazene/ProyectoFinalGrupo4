c963bf14a45d2ae34d0e34b07c8d7e84e6d5fe9c
AntonioPortela <gilantonio1c@gmail.com>
2019-02-14 18:20:00 +0100

    Prueba unitaria ernesto

--- coetusIV/src/test/java/com/example/spring/test/PruebaUnitariaListarPersona.java
+++ coetusIV/src/test/java/com/example/spring/test/PruebaUnitariaListarPersona.java
@@ -2,6 +2,8 @@ package com.example.spring.test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -13,6 +15,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 import com.example.spring.model.Persona;
 import com.example.spring.servicios.IServicios;
 
+/**
+ * 
+ * @author Grupo4
+ *
+ */
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -21,46 +28,19 @@ public class PruebaUnitariaListarPersona {
 	@Autowired
 	private IServicios serve;
 
-	private static final Logger logger = LoggerFactory.getLogger(PruebaUnitariaAddPersona.class);
-	
+	private static final Logger logger = LoggerFactory.getLogger(PruebaUnitariaListarPersona.class);
+
 	@Test
 	public void pruebaUnitariaListarPersona() throws Exception {// Realizo un test para demostrar que existe la lista
 
-		// Creo una persona
-		Persona persona = new Persona();
-		persona.setNombre("Hola");
-		persona.setApellido1("Soy la Prueba Test Listar");
-		persona.setApellido2("Estoy viva");
-		persona.setDni("159753852L");
+		try {
+			
+			List<Persona> lista1 = serve.list();
 
-		// Añado esa nueva persona a la bbdd
-		int cantidadInicial = serve.list().size();
-
-		serve.addPersona(persona);// Se realiza un add persona para determinar si la lista existe
-
-		int cantidadFinal = serve.list().size();
-
-		// Guardo su id en la variable id
-		int id = serve.existsAndGetId(persona);
-
-		// Si la persona existe
-
-		if (id != 1) {
-
-			try {
-				assertEquals(cantidadInicial + 1, cantidadFinal);
-				logger.info("Se ha añadido la nueva perosna correctamente, la BBDD existe");
-
-				// Elimino la nueva persona de la bbdd
-				serve.delPersona(id);
-
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		} else {
-			// La Lista no funciona
+		} catch (Exception e) {
 			assertEquals(true, false);
-			logger.info("La persona no existe en la bbdd, la lista no funciona o no existe");
+			logger.info("La lista no funciona o no existe");
+			e.printStackTrace();
 		}
 	}
 
